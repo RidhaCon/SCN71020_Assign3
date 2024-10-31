d932da93e48604090100aaefa0b91c02eef2874c
Ridha Alobied <Ralobied8865@conestogac.on.ca>
2024-10-30 20:39:50 -0400

    Production code Done

--- SCN71020_Assign3/FileName.c
+++ SCN71020_Assign3/FileName.c
@@ -12,3 +12,42 @@ int main(void)
 	return 0;
 }
 
+const char* result(const char* player1, const char* player2)
+{
+	char* Final;
+
+	if (player1 == player2)
+	{
+		Final = "Draw";
+	}
+	else if (player1 == "Rock" && player2 == "Paper")
+	{
+		Final = "Player2";
+	}
+	else if (player1 == "Rock" && player2 == "Scissors")
+	{
+		Final = "Player1";
+	}
+	else if (player1 == "Scissors" && player2 == "Paper")
+	{
+		Final = "Player1";
+	}
+	else if (player1 == "Scissors" && player2 == "Rock")
+	{
+		Final = "Player2";
+	}
+	else if (player1 == "Paper" && player2 == "Scissors")
+	{
+		Final = "Player2";
+	}
+	else if (player1 == "Paper" && player2 == "Rock")
+	{
+		Final = "Player1";
+	}
+	else
+	{
+		Final = "Invalid";
+	}
+
+	return Final;
+}
